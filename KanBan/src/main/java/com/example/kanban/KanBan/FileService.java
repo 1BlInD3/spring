fbe9d7014e94c0b8c43c5cad4e8be56779387136
Attila Bálind <attila.balind.repo@gmail.com>
2021-05-11 08:43:47 +0200

    html

--- KanBan/src/main/java/com/example/kanban/KanBan/FileService.java
+++ KanBan/src/main/java/com/example/kanban/KanBan/FileService.java
@@ -21,6 +21,8 @@ public class FileService {
             Files.copy(file.getInputStream(),copyLocation, StandardCopyOption.REPLACE_EXISTING);
         }catch(Exception e){
             e.printStackTrace();
+            throw new FileStorageException("Could not store file " + file.getOriginalFilename()
+                    + ". Please try again!");
         }
     }
 }
