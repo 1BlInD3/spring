cda24c34871b2b76ff8537152ff228e7ecf700d7
Attila Bálind <attila.balind.repo@gmail.com>
2021-06-28 11:52:03 +0200

    path beállítva request paraméterként

--- KanBan/src/main/java/com/example/kanban/KanBan/FileService.java
+++ KanBan/src/main/java/com/example/kanban/KanBan/FileService.java
@@ -1,11 +1,11 @@
 package com.example.kanban.KanBan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,21 +15,23 @@ import java.nio.file.StandardCopyOption;
 @RestController
 public class FileService {
 
-    public String uploadDir = "//10.0.0.11/TesztWeb/";
-
-    @PostMapping("/uploadFile")
-    public Response uploadFile(@RequestParam("file")MultipartFile file){
-        try{
-            Path copyLocation = Paths.get(uploadDir+ File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
-            Files.copy(file.getInputStream(),copyLocation, StandardCopyOption.REPLACE_EXISTING);
-            return new Response(false,"success",copyLocation.toString());
-        }catch(Exception e){
+    //public String uploadDir = "//10.0.0.11/TesztWeb/";
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileService.class);
+    @PostMapping("/uploadFile/{path}")
+    public Response uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("path") String path) {
+        try {
+            LOGGER.info("The path is: " + path);
+            Path copyLocation = Paths.get(path + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+            return new Response(false, "success", copyLocation.toString());
+        } catch (Exception e) {
             e.printStackTrace();
-            return new Response(true,e.toString(),"");
+            return new Response(true, e.toString(), "");
         }
     }
+
     @GetMapping("/test")
-    public Response endPointCheck(){
-        return new Response(false,"OK","Van endpoint");
+    public Response endPointCheck() {
+        return new Response(false, "OK", "Van endpoint");
     }
 }
